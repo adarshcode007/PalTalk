36b657588dd35920a58f093a901fae7662cb3f68
adarshcode007 <adarsh19pandey@gmail.com>
2025-11-07 17:42:17 +0530

    login refactoring

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,17 +9,3 @@ export const ENV = {
   EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
   CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
 };
-
-// PORT=3000
-// MONGO_URI=mongodb+srv://adarshpandey23:pass123abc@cluster0.kntwabz.mongodb.net/PalTalk_db?appName=Cluster0
-
-// NODE_ENV=development
-
-// JWT_SECRET=myjwtsecretkey
-
-// RESEND_API_KEY=re_b8MSsGUW_FDPoBTBzSq9EmbjgrY8Bw4UE
-
-// EMAIL_FROM="onboarding@resend.dev"
-// EMAIL_FROM_NAME="PalTalk Support"
-
-// CLIENT_URL=http://localhost:5173
