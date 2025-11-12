5499e5041e746c1e96224c4ea29692ea647e14e9
adarshcode007 <adarsh19pandey@gmail.com>
2025-11-12 19:52:58 +0530

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
